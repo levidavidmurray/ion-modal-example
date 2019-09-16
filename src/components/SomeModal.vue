1dd2ad94a0d680cf5c9dd857ab419a6bb3ce85c7
Levi <levi@cloudpractice.ca>
2019-09-16 12:52:43 -0700

    Init repo

---
+++ src/components/SomeModal.vue
@@ -0,0 +1,58 @@
+<template>
+	<ion-page>
+		<ion-header>
+			<ion-toolbar>
+				<ion-buttons slot="end">
+					<ion-button @click="dismissModal">
+						Close
+						<ion-icon slot="end" name="close"></ion-icon>
+					</ion-button>
+				</ion-buttons>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content>
+			<h3>I am a Modal!</h3>
+		</ion-content>
+	</ion-page>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    name: "SomeModal",
+    props: {
+      dismissCb: {
+        type: Function,
+        required: true,
+      },
+    },
+    methods: {
+      dismissModal()
+      {
+        this.dismissCb();
+      },
+    },
+  });
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+	h3 {
+		margin: 40px 0 0;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Some Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <h3>I present a modal!</h3>
+      <ion-button @click="createModal">Present Modal</ion-button>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import SomeModal from '@/components/SomeModal.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'home',
-  components: {
-    HelloWorld,
+
+  data()
+  {
+    return {
+      ionModal: null as HTMLIonModalElement,
+    }
   },
+
+  methods: {
+    createModal()
+    {
+      return this.$ionic.modalController
+          .create({
+            component: SomeModal,
+            showBackdrop: true,
+            componentProps: {
+              propsData: {
+                dismissCb: this.dismissModal,
+              }
+            }
+          })
+          .then((modal) => {
+            this.ionModal = modal;
+            this.ionModal.present();
+          })
+    },
+
+    dismissModal()
+    {
+      if (this.ionModal)
+      {
+        this.ionModal.dismiss()
+            .then(() => {
+              this.ionModal = null;
+            })
+      }
+    }
+  }
 });
 </script>
