244527becb7eef2287f49d2cfda3beda7761d6f7
Levi <levi@cloudpractice.ca>
2019-09-16 12:55:26 -0700

    Removed unused route

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import {IonicVueRouter} from '@ionic/vue';
-import Home from './views/Home.vue';
+import Home from './components/Home.vue';
 
 Vue.use(IonicVueRouter);
 
@@ -10,14 +10,6 @@ export default new IonicVueRouter({
       path: '/',
       name: 'home',
       component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
   ],
 });
